a58a30e7ddfa6930191dc775953775bc7ef4458d
RyanJ <ryanjj13579@gmail.com>
2026-08-26 12:04:25 -0700

    Candy Jar done

---
+++ CandyJarMain.java
@@ -0,0 +1,16 @@
+public class CandyJarMain {
+    public static void main(String[] args) {
+        CandyJar c1 = new CandyJar(15);
+        CandyJar c2 = new CandyJar(20);
+        
+        c1.eat(5);
+        c2.eat(10);
+
+        c1.refill(15);
+        c2.refill(20);
+        System.out.println("C1: ");
+        System.out.println("C2: ");
+    
+
+    }
+}
