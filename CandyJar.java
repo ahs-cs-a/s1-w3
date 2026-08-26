c0594e47755f809f1b84350ec16a87175f9be0cc
Rose Tomaro <rgtomaro@gmail.com>
2026-08-26 18:53:57 +0000

    y

--- CandyJar.java
+++ CandyJar.java
@@ -1,3 +1,18 @@
 public class CandyJar{
-    
+    private int numCandy;
+    public CandyJar(){
+        numCandy = 10;
+    }
+
+    public CandyJar(int a){
+        numCandy = a
+    }
+
+    public void eat(int nom){
+        numCandy -= nom
+    }
+
+    public void fill(int add){
+        numCandy += add
+    }
 }
---
+++ CandyJarMain.java
@@ -0,0 +1,4 @@
+public class CandyJarMain{
+    CandyJar c = new CandyJar();
+    CandyJar c1 = new CandyJar(507328967);
+}
