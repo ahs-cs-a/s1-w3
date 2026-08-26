bca0e98f3d60c3d764698fb7c8c1e96db8606b9c
Rose Tomaro <rgtomaro@gmail.com>
2026-08-26 19:18:30 +0000

    oui

--- CokeCooler.java
+++ CokeCooler.java
@@ -1,3 +1,22 @@
 public class CokeCooler {
-    
+    private int num;
+    public CokeCooler(){
+        num = 100;
+    }
+
+    public CokeCooler(int a){
+        num = a;
+    }
+
+    public void add(int b){
+        num += b;
+    }
+
+    public void sub(int c){
+        num -= c;
+    }
+
+    public void print(){
+        System.out.println("Number of Cokes: " + num);
+    }
 }
