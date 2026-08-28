55da46e8d59cf858fa2f75d4bc0022607294a98c
Rose Tomaro <rgtomaro@gmail.com>
2026-08-28 11:55:29 -0700

    Refillable

--- Refillable.java
+++ Refillable.java
@@ -1,3 +1,18 @@
 public class Refillable {
-    
+    private int amount;
+    public Refillable(){
+        amount =10;
+    }
+
+    public Refillable(int a){
+        amount = a;
+    }
+
+    public void change(int b){
+        amount += b;
+    }
+
+    public void print(){
+        System.out.println("Amount: " + amount);
+    }
 }
